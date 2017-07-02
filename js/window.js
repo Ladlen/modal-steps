b7a03601acc1592c7d77168b55918d56e60e2ae2
Dolgov Dmitriy <TwilightTower@mail.ru>
2017-07-03 02:48:08 +0300

    scroll to top

--- js/window.js
+++ js/window.js
@@ -29,6 +29,7 @@ jQuery(function ($) {
         if ($("[name=ans0]:checked").length > 0) {
             $("div[data-step=1]").hide();
             $("div[data-step=2]").show();
+            $(window).scrollTop(0);
         } else {
             alert("не выбран ни один пункт!");
         }
@@ -39,6 +40,7 @@ jQuery(function ($) {
         if ($("[name=ans1]:checked").length > 0) {
             $("div[data-step=2]").hide();
             $("div[data-step=3]").show();
+            $(window).scrollTop(0);
         } else {
             alert("не выбран ни один пункт!");
         }
@@ -46,46 +48,55 @@ jQuery(function ($) {
     $("div[data-step=2] .meedget_back_link").click(function () {
         $("div[data-step=2]").hide();
         $("div[data-step=1]").show();
+        $(window).scrollTop(0);
     });
 
     // Step 3
     $("div[data-step=3] .meedget_calc_next").click(function () {
         $("div[data-step=3]").hide();
         $("div[data-step=4]").show();
+        $(window).scrollTop(0);
     });
     $("div[data-step=3] .meedget_back_link").click(function () {
         $("div[data-step=3]").hide();
         $("div[data-step=2]").show();
+        $(window).scrollTop(0);
     });
 
     // Step 4
     $("div[data-step=4] .meedget_calc_next").click(function () {
         $("div[data-step=4]").hide();
         $("div[data-step=calc]").show();
+        $(window).scrollTop(0);
     });
     $("div[data-step=4] .meedget_back_link").click(function () {
         $("div[data-step=4]").hide();
         $("div[data-step=3]").show();
+        $(window).scrollTop(0);
     });
 
     // Step calc
     $("div[data-step=calc] .meedget_calc_next").click(function () {
         $("div[data-step=calc]").hide();
         $("div[data-step=phone]").show();
+        $(window).scrollTop(0);
     });
     $("div[data-step=calc] .meedget_back_link").click(function () {
         $("div[data-step=calc]").hide();
         $("div[data-step=4]").show();
+        $(window).scrollTop(0);
     });
 
     // Step phone
     $("div[data-step=phone] .meedget_calc_next").click(function () {
         $("div[data-step=phone]").hide();
         $("div[data-step=finish]").show();
+        $(window).scrollTop(0);
     });
     $("div[data-step=phone] .meedget_back_link").click(function () {
         $("div[data-step=phone]").hide();
         $("div[data-step=calc]").show();
+        $(window).scrollTop(0);
     });
 
     // Step phone
